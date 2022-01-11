ed1a54d5fa3549a9bf7142ae70b763bd647e0105
Ashish-Aggarwal007 <ashish.aggarwal.ec.miet@gmail.com>
2022-01-11 15:21:56 +0530

    E-store basic-template

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import data from "./data";
+import Product from "./components/Product";
+function App() {
+  return (
+    <div className = "grid-container">
+      <header className = "row">
+          <div>
+              <a className="brand" href = "index.html">E-Store</a>
+          </div>
+          <div>
+              <a href = "cart.html">Cart</a>
+              <a href="signin.html">Sign In</a>
+          </div>
+      </header>
+      <main>
+          <div className = "row center">
+           { data.products.map((product) => (
+               <Product key = {product._id} product = {product} />
+            ))}
+                           
+          </div>
+      </main>
+     <footer className = "row center"> All right reserved  </footer>
+    </div>
+  );
+}
+
+export default App;
