f005507e83d846dc422ee0874c76c535d1db5045
Ashish-Aggarwal007 <ashish.aggarwal.ec.miet@gmail.com>
2022-01-15 20:37:38 +0530

    changes to deploy

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-mongoose.connect("mongodb+srv://admin:1230@cluster0.86onw.mongodb.net/shop?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URL, {
     // to get rid of deprecated warnings
     useNewUrlParser:true,
     useUnifiedTopology: true,
@@ -45,9 +45,13 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
